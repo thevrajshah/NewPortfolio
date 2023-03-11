6ed4bbda0128f071b507c8f355b37fa87ed0a01e
Vraj Shah <vraj@bluelearn.in>
2023-03-12 02:21:26 +0530

    created a banger hero section

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,89 @@
+<slot />
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Golos+Text:ital,wght@0,400;0,700;1,400&display=swap');
+
+	:global(:root) {
+		--gradient-0: #291e3c;
+		--gradient-1: #22214d;
+		--gradient-2: #2d0d8d;
+		--gradient-3: #500d7e;
+		--gradient-4: #5403a5;
+	}
+
+	:global(*) {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	:global(html) {
+		font-size: 100%;
+	}
+
+	:global(body) {
+		font-weight: 400;
+		font-family: 'Golos Text', serif;
+		line-height: 1.75;
+		color: hsl(281, 24%, 80%);
+		background: var(--gradient-1);
+		box-sizing: border-box;
+	}
+
+	:global(.container) {
+		margin: 0 auto;
+		max-width: 80rem;
+		padding: 0 1rem;
+	}
+
+	:global(p) {
+		margin-bottom: 1rem;
+	}
+
+	:global(a) {
+		text-decoration: none;
+		color: hsl(281, 100%, 87%);
+	}
+	:global(a:hover) {
+		text-decoration: underline;
+		color: hsl(281, 100%, 80%);
+	}
+
+	:global(h1, h2, h3, h4, h5) {
+		/* margin: 3rem 0 1.38rem; */
+		/* font-family: 'Poppins', sans-serif; */
+		font-weight: 400;
+		line-height: 1.3;
+	}
+
+	:global(h1) {
+		margin-top: 0;
+		font-size: 7.594rem;
+	}
+
+	:global(h2) {
+		font-size: 5.063rem;
+	}
+
+	:global(h3) {
+		font-size: 3.375rem;
+	}
+
+	:global(h4) {
+		font-size: 2.25rem;
+	}
+
+	:global(h5) {
+		font-size: 1.5rem;
+	}
+
+	:global(small) {
+		font-size: 0.667rem;
+	}
+
+	@media only screen and (max-width: 600px) {
+		:global(html) {
+			font-size: 80%;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,77 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { blur } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+</script>
+
+<main id="hero">
+	<div class="container">
+		<div class="name">
+			<h5><b>vraj</b>shah</h5>
+			<p>
+				(works at <a href="http://www.bluelearn.in" target="_blank" rel="noopener noreferrer"
+					>bluelearn</a
+				>)
+			</p>
+		</div>
+		<p
+			in:blur={{ amount: 50, easing: quintOut, duration: 500 }}
+			out:blur={{ amount: 50, easing: quintOut, duration: 500 }}
+			class="title"
+		>
+			portfolio website or smth, idk
+		</p>
+		<div class="quote">
+			<h5>just leaving my div here, can you look after it? brb!</h5>
+			<h2>
+				<b>
+					{'<div>'}
+				</b>
+			</h2>
+			<!-- <p>
+				(works at <a href="http://www.bluelearn.in" target="_blank" rel="noopener noreferrer"
+					>bluelearn</a
+				>)
+			</p> -->
+		</div>
+	</div>
+</main>
+
+<style>
+	#hero {
+		min-height: 100vh;
+		background-image: url("data:image/svg+xml,%3C!-- svg: first layer --%3E%3Csvg viewBox='0 0 250 250' opacity='0.2' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='1000' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E"),
+			radial-gradient(
+				circle at top right,
+				var(--gradient-4) 16%,
+				var(--gradient-3) 32%,
+				var(--gradient-2) 48%,
+				var(--gradient-1) 64%,
+				var(--gradient-0) 100%
+			);
+	}
+
+	#hero .container {
+		min-height: inherit;
+		position: relative;
+		padding-top: 1.2rem;
+		padding-bottom: 1.2rem;
+	}
+
+	#hero .title {
+		position: absolute;
+		top: 1.2rem;
+	}
+	#hero .name {
+		position: absolute;
+		top: 1.2rem;
+		right: 1rem;
+		text-align: right;
+	}
+	#hero .quote {
+		position: absolute;
+		bottom: 2rem;
+		right: 1rem;
+		max-width: 54rem;
+		text-align: right;
+	}
+</style>
